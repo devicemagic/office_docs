2f937a23e59eb314dda7ccbdc7976dbc5d93c223
Javar Alexander <javar.alexander@gmail.com>
2024-02-25 18:03:32 -0500

    uncomment out other tests, passing 100%

--- test/word/test_if_else_replacer.rb
+++ test/word/test_if_else_replacer.rb
@@ -15,68 +15,68 @@ class IfElseReplacerTest < Test::Unit::TestCase
   BROKEN_IF = File.join(File.dirname(__FILE__), '..', 'content', 'template', 'if_else', 'broken_if_test.docx')
 
   include IfElseInParagraphTest
-  #include IfElseOverParagraphsTest
-  #include IfElseTableRowTest
+  include IfElseOverParagraphsTest
+  include IfElseTableRowTest
 
-  #
-#   def test_get_placeholders
-#     doc = Office::WordDocument.new(IN_SAME_PARAGRAPH_IF_ELSE)
-#     template = Word::Template.new(doc)
-#     placeholders = template.get_placeholders
+  
+  def test_get_placeholders
+    doc = Office::WordDocument.new(IN_SAME_PARAGRAPH_IF_ELSE)
+    template = Word::Template.new(doc)
+    placeholders = template.get_placeholders
 
-#     correct_placeholder_info = [{:placeholder_text=>"{% if fields.a %}",
-#   :paragraph_index=>2,
-#   :beginning_of_placeholder=>{:run_index=>0, :char_index=>0},
-#   :end_of_placeholder=>{:run_index=>0, :char_index=>16}},
-#  {:placeholder_text=>"{% endif %}",
-#   :paragraph_index=>2,
-#   :beginning_of_placeholder=>{:run_index=>0, :char_index=>32},
-#   :end_of_placeholder=>{:run_index=>0, :char_index=>42}},
-#  {:placeholder_text=>"{% if fields.b = “pie” %}",
-#   :paragraph_index=>4,
-#   :beginning_of_placeholder=>{:run_index=>0, :char_index=>0},
-#   :end_of_placeholder=>{:run_index=>0, :char_index=>24}},
-#  {:placeholder_text=>"{% endif %}",
-#   :paragraph_index=>4,
-#   :beginning_of_placeholder=>{:run_index=>0, :char_index=>34},
-#   :end_of_placeholder=>{:run_index=>0, :char_index=>44}},
-#  {:placeholder_text=>"{% if fields.b != “pie” %}",
-#   :paragraph_index=>6,
-#   :beginning_of_placeholder=>{:run_index=>0, :char_index=>0},
-#   :end_of_placeholder=>{:run_index=>0, :char_index=>25}},
-#  {:placeholder_text=>"{% endif %}",
-#   :paragraph_index=>6,
-#   :beginning_of_placeholder=>{:run_index=>0, :char_index=>39},
-#   :end_of_placeholder=>{:run_index=>0, :char_index=>49}},
-#  {:placeholder_text=>"{% if fields.c == “lol” %}",
-#   :paragraph_index=>8,
-#   :beginning_of_placeholder=>{:run_index=>0, :char_index=>0},
-#   :end_of_placeholder=>{:run_index=>1, :char_index=>9}},
-#  {:placeholder_text=>"{% endif %}",
-#   :paragraph_index=>8,
-#   :beginning_of_placeholder=>{:run_index=>1, :char_index=>22},
-#   :end_of_placeholder=>{:run_index=>1, :char_index=>32}}]
+    correct_placeholder_info = [{:placeholder_text=>"{% if fields.a %}",
+  :paragraph_index=>2,
+  :beginning_of_placeholder=>{:run_index=>0, :char_index=>0},
+  :end_of_placeholder=>{:run_index=>0, :char_index=>16}},
+ {:placeholder_text=>"{% endif %}",
+  :paragraph_index=>2,
+  :beginning_of_placeholder=>{:run_index=>0, :char_index=>32},
+  :end_of_placeholder=>{:run_index=>0, :char_index=>42}},
+ {:placeholder_text=>"{% if fields.b = “pie” %}",
+  :paragraph_index=>4,
+  :beginning_of_placeholder=>{:run_index=>0, :char_index=>0},
+  :end_of_placeholder=>{:run_index=>0, :char_index=>24}},
+ {:placeholder_text=>"{% endif %}",
+  :paragraph_index=>4,
+  :beginning_of_placeholder=>{:run_index=>0, :char_index=>34},
+  :end_of_placeholder=>{:run_index=>0, :char_index=>44}},
+ {:placeholder_text=>"{% if fields.b != “pie” %}",
+  :paragraph_index=>6,
+  :beginning_of_placeholder=>{:run_index=>0, :char_index=>0},
+  :end_of_placeholder=>{:run_index=>0, :char_index=>25}},
+ {:placeholder_text=>"{% endif %}",
+  :paragraph_index=>6,
+  :beginning_of_placeholder=>{:run_index=>0, :char_index=>39},
+  :end_of_placeholder=>{:run_index=>0, :char_index=>49}},
+ {:placeholder_text=>"{% if fields.c == “lol” %}",
+  :paragraph_index=>8,
+  :beginning_of_placeholder=>{:run_index=>0, :char_index=>0},
+  :end_of_placeholder=>{:run_index=>1, :char_index=>9}},
+ {:placeholder_text=>"{% endif %}",
+  :paragraph_index=>8,
+  :beginning_of_placeholder=>{:run_index=>1, :char_index=>22},
+  :end_of_placeholder=>{:run_index=>1, :char_index=>32}}]
 
-#     placeholders.each do |p|
-#       p.delete(:paragraph_object)
-#     end
+    placeholders.each do |p|
+      p.delete(:paragraph_object)
+    end
 
-#     assert correct_placeholder_info == placeholders
-#   end
+    assert correct_placeholder_info == placeholders
+  end
 
-#   def test_broken_placeholder_template
-#     doc = Office::WordDocument.new(BROKEN_IF)
-#     template = Word::Template.new(doc)
-#     raised_error = false
-#     begin
-#       template.render({some: {cool_heading: 'test'}})
-#     rescue => e
-#       raised_error = true
-#       assert(e.is_a?(Liquid::ArgumentError))
-#       assert(e.message.include?("Error in {% if f fields.lol %}..{% endif %}"))
-#     end
-#     assert(raised_error)
-#   end
+  def test_broken_placeholder_template
+    doc = Office::WordDocument.new(BROKEN_IF)
+    template = Word::Template.new(doc)
+    raised_error = false
+    begin
+      template.render({some: {cool_heading: 'test'}})
+    rescue => e
+      raised_error = true
+      assert(e.is_a?(Liquid::ArgumentError))
+      assert(e.message.include?("Error in {% if f fields.lol %}..{% endif %}"))
+    end
+    assert(raised_error)
+  end
 
   private
 
